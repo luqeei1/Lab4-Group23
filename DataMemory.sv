5a85bf8509be00d5fc1a516e0799a5213ac90864
luqeei1 <akarshgopalam@gmail.com>
2024-11-28 19:50:34 +0000

    mux added for branch

--- DataMemory.sv
+++ DataMemory.sv
@@ -5,30 +5,34 @@ module DataMemory #(
 
 )(
     input logic [1:0]            address_mode,
+    input logic                     ResultSrc,
     input logic [WIDTH-1:0]      A,
     input logic [WIDTH-1:0]      WD,
     input logic                  WE,
-    output logic [WIDTH-1:0]     RD
+    output logic [WIDTH-1:0]     Result
     
 
 
 );
 
     logic [7:0] ram_array [2**17 -1:0];
+    logic [WIDTH-1:0] RD;
 
 always_ff @(posedge clk) 
     begin
     if (wen == 1'b1)
         ram_array[ALUout] <= WriteData;    // CHANGE TO single byte addressing
+        RD = ram_array[ALUout];
     else
         ram_array[ALUout] <= ram_array[ALUout];
+        RD = ram_array[ALUout];
+    end 
+
+    begin 
+        case(ResultSrc)
+            0'b1 : Result <= RD;
+            0'b0 : Result <= A;
     end 
 
 
-assign ReadData = ram_array[ALUout];
-
 endmodule
-
-
-
-
